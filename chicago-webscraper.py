25cd03c0d32f972753f0190f371a2853e7fd1b7e
enginneenneer <viable.projects.found@gmail.com>
2017-04-22 22:38:01 +0300

    Update chicago-webscraper.py

--- chicago-webscraper.py
+++ chicago-webscraper.py
@@ -2,157 +2,113 @@
 Searches on a specific site in Chicago, if new viable projects have been added.
 Viable projects are Office with 25ksf and Industrial with 90ksf
 Sends Mail if new projects have been found or not.
-
 """
-
 
 import requests
 import smtplib
+
 from bs4 import BeautifulSoup
 
 
-r = requests.get("http://interactive.chicagobusiness.com/closer/constructionpipeline/private/full/")
-
-soup = BeautifulSoup(r.content)
-officeResults = soup.find_all(lambda tag: "office" in tag.string if tag.string else False)
-listOffice = map(lambda element: element.string, officeResults)
-
-indResults = soup.find_all(lambda tag: "industrial" in tag.string if tag.string else False)
-listInd = map(lambda element: element.string, indResults)
-
-viableOfficeList = []
-viableIndustrialList = []
+def get_projects(buildingType, soup):
+    results = soup.find_all(lambda tag: buildingType in tag.string if tag.string else False)
+    return map(lambda element: element.string, results)
 
 
-with open("nr_of_office.txt") as fileOffice:
+def projects_checked_already(filename):
+    with open(filename) as file:
+        nrStr = file.readlines()
+        return int(nrStr[0])
+
+
+def get_viable_projects(projects, limit):
     """
-    Reads the number of viable projects from the last time the program ran, from a text file.
-    It will be the only element from a list, and I will save it as int to compare it later.
+    Example: 3,396-square-foot medical office
+    It will only get those projects where the "office" or "industrial" string is found.
+    Then it will break up so that it get's the square foot number, which is a string
+    and it will be transformed in an int.
+    If it's viable it will be appended to the corresponding list.
     """
-    nrOfficeStr = fileOffice.readlines()
-    nrOffice = int(nrOfficeStr[0])
+    viables = []
+    for bldg in projects:
+        """
+        Transforms the string in int, to verify square feet,
+        then adds it to the list if it's viable
+        """
+        x = bldg.split(', ')
+        y = x[0].split('-square')
+        z = y[0].split(',')
+        x = ""
+        for el in z:
+            x += el
+
+        sqFeet = 0
+        try:
+            sqFeet = int(x)
+        except ValueError as errorStringToInt:
+            pass
+
+        if sqFeet > limit:
+            viables.append(bldg)
+    return viables
 
 
-with open("nr_of_industrial.txt") as fileIndustrial:
+def get_mail_message(viables, nrBldgs, filename):
     """
-    Reads the number of viable projects from the last time the program ran, from a text file.
-    It will be the only element from a list, and I will save it as int to compare it later.
+    It compares the length of the list with the length of the list from the previous run,
+    which is always saved in the file. If there are more then an e-mail will be sent
+    with the new projects that appeared.
     """
-    nrIndStr = fileIndustrial.readlines()
-    nrIndustrial = int(nrIndStr[0])
+    message = ""
+    if len(viables) == nrBldgs:
+        message += "0 \n"
+    else:
+        nrNewProjects = len(viables) - nrBldgs
 
-"""
-Example: 3,396-square-foot medical office 
-It will only get those projects where the "office" or "industrial" string is found.
-Then it will break up so that it get's the square foot number, which is a string
-and it will be transformed in an int.
-If it's viable it will be appended to the corresponding list.
-"""
-for bldg in listOffice:
+        for i in range(nrNewProjects):
+            message += viables[i] + "\n"
+
+        file = open(filename, "w")
+        file.write(str(len(viables)))
+    return message
+
+
+def send_mail(message):
     """
-    Transforms the string in int, to verify square feet,
-    then adds it to the list if it's viable
+    Sending a mail if new projects have been found or not.
+    Enter two e-mails, one from which the mail will be sent,
+    and another to receive it.
     """
-    x = bldg.split(', ')
-    y = x[0].split('-square')
-    z = y[0].split(',')
-    x = ""
-    for el in z:
-        x += el
-
-    sq_feet = 0
-    try:
-        sq_feet = int(x)
-    except ValueError as errorStringToInt:
-        pass
-
-    if sq_feet > 24999:
-        viableOfficeList.append(bldg)
-
-"""
-240,000-square-foot industrial center, $25 million 
-It will only get those projects where the "office" or "industrial" string is found.
-Then it will break up so that it get's the square foot number, which is a string
-and it will be transformed in an int.
-If it's viable it will be appended to the corresponding list.
-"""
-for bldg in listInd:
-    """
-    Transforms the string in int, to verify square feet,
-    then adds it to the list if it's viable
-    """
-    x = bldg.split(', ')
-    y = x[0].split('-square')
-    z = y[0].split(',')
-    x = ""
-    for el in z:
-        x += el
-
-    sq_feet = 0
-    try:
-        sq_feet = int(x)
-    except ValueError as errorStringToInt:
-        pass
-
-    if sq_feet > 89999:
-        viableIndustrialList.append(bldg)
+    emailSend = input("Enter email to send the projects: ")
+    passEmail = input("Enter password: ")
+    emailReceive = input("To whom: ")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(emailSend, passEmail)
+    server.sendmail(emailSend, emailReceive, message)
+    server.quit()
 
 
-mailMessageOffice = "Office: "
+def main():
+    r = requests.get("http://interactive.chicagobusiness.com/closer/constructionpipeline/private/full/")
+    soup = BeautifulSoup(r.content)
 
-"""
-It compares the length of the list with the length of the list from the previous run,
-which is always saved in the file. If there are more then an e-mail will be sent
-with the new projects that appeared.
-"""
-if len(viableOfficeList) == nrOffice:
-    mailMessageOffice += "0 \n"
-else:
-    nrNewOfficeProjects = len(viableOfficeList) - nrOffice
-    
-    for i in range(nrNewOfficeProjects):
-        mailMessageOffice += viableOfficeList[i] + "\n"
+    offices = get_projects("office", soup)
+    industrials = get_projects("industrial", soup)
 
-    fileOffice = open("nr_of_office.txt", "w")
-    fileOffice.write(str(len(viableOfficeList)))
+    nrOffice = projects_checked_already("nr_of_office.txt")
+    nrIndustrial = projects_checked_already("nr_of_industrial.txt")
 
-mailMessageInd = ""
-mailMessageInd += "Industrial: "
+    viableOffices = get_viable_projects(offices, 24999)
+    viableIndustrials = get_viable_projects(industrials, 89999)
 
+    mailMessageOffice = "\nOffice:\n"
+    mailMessageOffice += get_mail_message(viableOffices, nrOffice, "nr_of_office.txt")
 
-if len(viableIndustrialList) == nrIndustrial:
-    mailMessageInd += "0 \n"
-else:
-    nrNewIndustrialProjects = len(viableIndustrialList) - nrIndustrial
+    mailMessageInd = "\nIndustrial:\n"
+    mailMessageInd += get_mail_message(viableIndustrials, nrIndustrial, "nr_of_industrial.txt")
 
-    for i in range(nrNewIndustrialProjects):
-        mailMessageInd += viableIndustrialList[i] + "\n"
+    mailMessage = mailMessageOffice + mailMessageInd
+    send_mail(mailMessage)
 
-    fileInd = open("nr_of_industrial.txt", "w")
-    fileInd.write(str(len(viableIndustrialList)))
-
-
-mailMessage = "Office: " + mailMessageOffice + mailMessageInd
-
-
-"""
-Sending a mail if new projects have been found or not.
-Enter two e-mails, one from which the mail will be sent,
-and another to receive it.
-"""
-emailSend = input("Enter email to send the projects: ")
-passEmail = input("Enter password: ")
-emailReceive = input("To whom: ")
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(emailSend, passEmail)
-server.sendmail(emailSend, emailReceive, mailMessage)
-server.quit()
-
-
-
-
-
-
-
-
+main()
